ff24c387457062f9e29cd1a298f5fe7ba5e8b7dc
malefaro <malefaro@gmail.com>
2018-05-28 20:31:02 +0300

    hw2 in process

--- plots.py
+++ plots.py
@@ -21,37 +21,5 @@ print("CoolProp version:", CP.get_global_param_string("version"))
 # plot.calc_isolines(CoolProp.iHmass, iso_range=[h1,h2], num = 5 )
 # plot.show()
 
-print(math.log(15/4,math.e))
-
-h3 = 184590
-p = 1 * 10**6
-print(CP.PropsSI('T','H',h3,'P',p,'Nitrogen'))
-print(CP.PropsSI('GAS_CONSTANT', 'Nitrogen'))
-
-
-# plot = PropertyPlot('Air', 'TS', unit_system='SI', tp_limits='ACHP')
-# plot.set_axis_limits()
-
-import numpy as np
-import matplotlib.pyplot as plt
-# x=np.arange(-10,10.01,0.01) #Массив значений аргумента
-x1 = [1,2,3,4,5,6]
-y1 = [1,4,9,16,25,36]
-#
-# x = np.add(x1,x1)
-#
-# plt.plot(x1,y1) #Построение графика
-# plt.xlabel(r'$x$') #Метка по оси x в формате TeX
-# plt.ylabel(r'$f(x)$') #Метка по оси y в формате TeX
-# plt.title(r'$y=x^2$') #Заголовок в формате TeX
-# plt.grid(True) #Сетка
-# plt.show() #Показать график
-
-plt.su
-plt.plot(x1, y1)
-plt.twinx()
-plt.show()
-
-x = [x for x in range(100,210,10)]
-print(x)
+# print(CP.PropsSI('S', ))
 
